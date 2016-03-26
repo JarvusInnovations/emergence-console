b89da4f7dc45d7d2b3078653b866a3c8dece3acf
Bill Clinton <BillClinton@users.noreply.github.com>
2016-03-26 00:53:25 -0400

    Add Viewport

--- sencha-workspace/EmergenceConsole/app.js
+++ sencha-workspace/EmergenceConsole/app.js
@@ -9,15 +9,15 @@ Ext.application({
     extend: 'EmergenceConsole.Application',
 
     requires: [
-        'EmergenceConsole.view.main.Main'
+        'EmergenceConsole.view.Viewport'
     ],
 
     // The name of the initial view to create. With the classic toolkit this class
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
     //
-    mainView: 'EmergenceConsole.view.main.Main'
-	
+    mainView: 'EmergenceConsole.view.Viewport'
+
     //-------------------------------------------------------------------------
     // Most customizations should be made to EmergenceConsole.Application. If you need to
     // customize this file, doing so below this section reduces the likelihood
