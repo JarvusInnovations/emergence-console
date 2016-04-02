2a3aa9386b11a2f407eb4566aa416deabfb10ccc
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-02 14:45:51 -0400

    Update menu toolbar, getting rid of nav tabpanel

--- sencha-workspace/EmergenceConsole/app/view/sites/Menu.js
+++ sencha-workspace/EmergenceConsole/app/view/sites/Menu.js
@@ -3,28 +3,30 @@ Ext.define('EmergenceConsole.view.sites.Menu', {
     extend: 'Ext.toolbar.Toolbar',
     xtype: 'sites-menu',
 
-    vertical: true,
+//    vertical: true,
+    defaults: {
+        scale: 'small',
+        iconAlign: 'top'
+    },
 
     items: [{
         text: 'Changes',
-        iconCls: 'x-fa fa-home',
-        scale: 'medium',
-        iconAlign: 'top'
+        iconCls: 'x-fa fa-area-chart',
+        route: 'sites/changes'
     },{
         text: 'Files',
-        iconCls: 'x-fa fa-home',
-        scale: 'medium',
-        iconAlign: 'top'
+        iconCls: 'x-fa fa-files-o',
+        route: 'sites/files'
     },{
         text: 'Sources',
-        iconCls: 'x-fa fa-home',
-        scale: 'medium',
-        iconAlign: 'top'
+        iconCls: 'x-fa fa-cloud-download'
+    },{
+        text: 'Shell',
+        iconCls: 'x-fa fa-square'
     },{
         text: 'Docs',
-        iconCls: 'x-fa fa-home',
-        scale: 'medium',
-        iconAlign: 'top'
+        iconCls: 'x-fa fa-info-circle',
+        route: 'sites/docs'
     }]
 
 });
