0e84d80296a6fd1522c44797cd74d91a37b25525
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-02 14:44:16 -0400

    Add apikits and atheme to save eye strain for now

--- sencha-workspace/packages/get-packages.sh
+++ sencha-workspace/packages/get-packages.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
+git clone https://github.com/JarvusInnovations/jarvus-apikit.git
+git clone https://github.com/JarvusInnovations/emergence-apikit.git
 git clone https://github.com/JarvusInnovations/jarvus-ace.git
+
+
+# A theme to ease eye strain during development
+git clone https://github.com/BillClinton/theme-dev-dark.git
+
