168e570750a7a458eb9cedfa8e37f5b233bd2679
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-03 14:14:12 -0400

    Add temporary fix for idParam issue
    See https://www.sencha.com/forum/showthread.php?303751-Setting-idParam-or-nodeParam-to-null-gets-it-cast-to-a-string

--- sencha-workspace/EmergenceConsole/app/proxy/TreeRecords.js
+++ sencha-workspace/EmergenceConsole/app/proxy/TreeRecords.js
@@ -1,6 +1,18 @@
+/*jslint browser: true, undef: true *//*global Ext*/
 Ext.define('EmergenceConsole.proxy.TreeRecords', {
     extend: 'Emergence.proxy.Records',
-    alias: 'proxy.treerecords'
+    alias: 'proxy.treerecords',
 
+    buildRequest: function(operation) {
+        var me = this,
+            request = me.superclass.superclass.buildRequest.call(me,operation),
+            params = request.getParams();
+
+        if (params) {
+            delete params[me.getIdParam()];
+        }
+
+        return request;
+    }
 
 });
