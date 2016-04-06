3d61b2991245b4353981ac179ba47ea022d805fc
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-06 13:28:55 -0400

    Add custom connction and proxies
    This is working code. I expect it to change

---
+++ sencha-workspace/EmergenceConsole/app/proxy/WebDavAPI.js
@@ -0,0 +1,76 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('EmergenceConsole.proxy.WebDavAPI', {
+    extend: 'Ext.data.Connection',
+    singleton: true,
+
+    qualifiedUrlRe: /^(https?:)?\/\//,
+
+    config: {
+        /**
+         * @cfg {String/null}
+         * A host to prefix URLs with, or null to leave paths domain-relative
+         */
+        host: null,
+
+        /**
+         * @cfg {Boolean}
+         * True to use HTTPS when prefixing host. Only used if {@link #cfg-host} is set
+         */
+        useSSL: false,
+
+        /**
+         * @cfg {String/null}
+         * A path to prefix URLs with
+         */
+        pathPrefix: null,
+
+        // @inheritdoc
+        withCredentials: true,
+
+        // @inheritdoc
+        //useDefaultXhrHeader: false,
+
+        // @inheritdoc
+        disableCaching: false
+    },
+
+    //@private
+    buildUrl: function(path) {
+        var me = this,
+            host = me.getHost(),
+            pathPrefix = me.getPathPrefix();
+
+        if (me.qualifiedUrlRe.test(path)) {
+            return path;
+        }
+
+        if (pathPrefix) {
+            path = pathPrefix + path;
+        }
+
+        if (host) {
+            path = (me.getUseSSL() ? 'https://' : 'http://') + host + '/develop/' +path;
+        }
+
+        return path;
+    },
+
+
+    getFile: function(path, cb) {
+        var me = this;
+
+        me.request({
+            method: 'get',
+            url: me.buildUrl(path),
+            headers: {
+                'Accept': '*/*'
+            },
+            success: function(response) {
+                var text = response.responseText,
+                    contentType = response.getResponseHeader('content-type');
+
+                cb.call(this,path,text,contentType);
+            }
+        });
+    }
+});
