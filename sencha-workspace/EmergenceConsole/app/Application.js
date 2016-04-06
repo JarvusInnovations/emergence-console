73f430e6b0f0f328279972be75f08a79095b1110
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-06 13:24:07 -0400

    Add webdav proxy

--- sencha-workspace/EmergenceConsole/app/Application.js
+++ sencha-workspace/EmergenceConsole/app/Application.js
@@ -1,3 +1,4 @@
+/*jslint browser: true, undef: true *//*global Ext,EmergenceConsole*/
 /**
  * The main application class. An instance of this class is created by app.js when it
  * calls Ext.application(). This is the ideal place to handle application launch and
@@ -7,6 +8,11 @@ Ext.define('EmergenceConsole.Application', {
     extend: 'Ext.app.Application',
 
     name: 'EmergenceConsole',
+
+    requires: [
+        'EmergenceConsole.proxy.API',
+        'EmergenceConsole.proxy.WebDavAPI'
+    ],
 
     controllers: [
         'Sites',
@@ -24,7 +30,12 @@ Ext.define('EmergenceConsole.Application', {
         var pageParams = Ext.Object.fromQueryString(location.search);
 
         if (pageParams.apiHost) {
-            Emergence.util.API.setHost(pageParams.apiHost);
+            EmergenceConsole.proxy.API.setHost(pageParams.apiHost);
+            EmergenceConsole.proxy.WebDavAPI.setHost(pageParams.apiHost);
+        } else {
+            // TODO: delete this
+            EmergenceConsole.proxy.API.setHost('test.emergence.local');
+            EmergenceConsole.proxy.WebDavAPI.setHost('test.emergence.local');
         }
     },
 
