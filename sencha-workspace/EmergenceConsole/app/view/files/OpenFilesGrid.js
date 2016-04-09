209953bba9a3459b87033b3a627862e17423071e
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-09 05:25:42 -0400

    Add actioncolumns to open files grid

--- sencha-workspace/EmergenceConsole/app/view/files/OpenFilesGrid.js
+++ sencha-workspace/EmergenceConsole/app/view/files/OpenFilesGrid.js
@@ -3,14 +3,35 @@ Ext.define('EmergenceConsole.view.files.OpenFilesGrid', {
     extend: 'Ext.grid.Panel',
     xtype: 'files-openfilesgrid',
 
-    store: 'files.FileRefs',
-
     hideHeaders: true,
     viewConfig: {
         loadMask: false
     },
 
+    store: {
+        fields: ['fileName','path','editorId','dirty'],
+        sorters: ['fileName']
+    },
+
     columns: [{
+        xtype:'actioncolumn',
+        width: 18,
+        items: [{
+            iconCls: 'x-fa fa-times',
+            action: 'closefile'
+        }]
+    },{
+        xtype:'actioncolumn',
+        width: 18,
+        items: [{
+            iconCls: 'x-fa fa-floppy-o',
+            action: 'savefile',
+            disabledCls: 'x-item-hidden', // TODO: not working
+            isDisabled: function(view,rowIndex,colIndex,item,rec) {
+                return !rec.data.dirty;
+            }
+        }]
+    },{
         text: 'Name',
         dataIndex: 'fileName',
         flex: 1
