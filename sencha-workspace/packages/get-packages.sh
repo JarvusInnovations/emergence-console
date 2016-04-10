cf621642379ddf034888f021ac23f3cfaa27e481
Bill Clinton <BillClinton@users.noreply.github.com>
2016-04-10 17:37:39 -0400

    Switch dev theme

--- sencha-workspace/packages/get-packages.sh
+++ sencha-workspace/packages/get-packages.sh
@@ -6,5 +6,6 @@ git clone https://github.com/JarvusInnovations/jarvus-ext-actionevents.git
 
 
 # A theme to ease eye strain during development
-git clone -b EmergenceConsole https://github.com/BillClinton/theme-dev-dark.git
+#git clone -b EmergenceConsole https://github.com/BillClinton/theme-dev-dark.git
+git clone https://github.com/BillClinton/theme-dev-dark-classic.git
 
