8c63318e91f6f37b4191bab6c01acbf38ce8e470
Bill Clinton <BillClinton@users.noreply.github.com>
2016-05-05 20:59:45 -0400

    Remove unused config option

--- sencha-workspace/EmergenceConsole/app/view/files/EditorContainer.js
+++ sencha-workspace/EmergenceConsole/app/view/files/EditorContainer.js
@@ -3,12 +3,6 @@ Ext.define('EmergenceConsole.view.files.EditorContainer', {
     extend: 'Ext.Panel',
     xtype: 'files-editorcontainer',
 
-/*
-    config: {
-        openFiles: []
-    },
-*/
-
     layout: 'card',
 
     dockedItems: [{
